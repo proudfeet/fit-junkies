45d27add16ed86d1054949ad5e96b22f5cdbf686
robynlarsen <r@robynlarsen.ca>
2015-03-29 12:34:39 -0400

    workouts rakedb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150329160937) do
+ActiveRecord::Schema.define(version: 20150329162357) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "sets"
@@ -50,5 +50,12 @@ ActiveRecord::Schema.define(version: 20150329160937) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "workouts", force: :cascade do |t|
+    t.string   "workout_type"
+    t.text     "notes"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
 end
