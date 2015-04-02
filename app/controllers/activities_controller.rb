505dda813e9f14cc035de6ba1df4820fb4daffd8
proudfeet <simon.d.proudfoot@gmail.com>
2015-04-02 00:15:39 -0400

    - create, activity_params, edit, and update actions created in activities controller - index includes edit link - added labels and inputs for all tables in _form

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -7,4 +7,28 @@ class ActivitiesController < ApplicationController
   	@activity = Activity.new
   end
 
+  def create
+  	@activity = Activity.new(activity_params)
+
+  	if @activity.save
+  		redirect_to '/activities'
+  	else
+  		render 'new'
+  	end
+  end
+
+  def activity_params
+  	params.require(:activity).permit(:name, :sets, :repetition, :other)
+  end
+
+  def edit
+  	@activity = Activity.find(params[:id])
+  	render 'new'
+  end
+
+  def update
+  	@activity = Activity.find(params[:id])
+  	@activity.update(activity_params)
+  	redirect_to '/activities'
+  end
 end
