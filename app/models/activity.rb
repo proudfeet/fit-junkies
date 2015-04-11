f9aee287f64d5b13b0be9f73c243856db0346dfb
robynlarsen <r@robynlarsen.ca>
2015-04-10 21:11:56 -0400

    create activity, workout, user association

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,5 @@
 class Activity < ActiveRecord::Base
-  belongs_to :workouts
+  # belongs_to :workouts
+
+  has_and_belongs_to_many :workouts
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  # before_validation { name.strip! } --> used to validate
+  has_many :workouts
+  # has_many :activites, through :workouts
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
 end
