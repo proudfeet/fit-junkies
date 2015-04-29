ac5b290afaef5aa42f11920a660eb9562a4b5cfb
Robyn Larsen <r@robynlarsen.ca>
2015-04-29 00:02:11 -0400

    Workout activities belongs to activities and workouts, allow sets reps etc. to blank

--- app/models/workout_activity.rb
+++ app/models/workout_activity.rb
@@ -1,15 +1,20 @@
 class WorkoutActivity < ActiveRecord::Base
-  has_many :workouts, through: :workout_activities
-  has_many :activities, through: :workout_activities
-  validates :workout_id, presence: true
-  validates :activity_id, presence: true
-  validates :sets, numericality: true
-  validates :reps, numericality: true
-  validates :weight, numericality: true
-  validates :distance, numericality: true
-  validates :time, numericality: true
+  # has_many :workouts, through: :workout_activities
+  # has_many :activities, through: :workout_activities
+  belongs_to :workout
+  belongs_to :activity
+  validates :workout, presence: true
+  validates :activity, presence: true
+  validates :sets, numericality: {allow_blank: true}
+  validates :reps, numericality: {allow_blank: true}
+  validates :weight, numericality: {allow_blank: true}
+  validates :distance, numericality: {allow_blank: true}
+  validates :time, numericality: {allow_blank: true}
 
   def getActivityName
   	Activity.find(self.activity_id).name
   end
+
+  # if you call an activity on a workout it will proxy name to the activity
+  delegate :name, to: :activity
 end
