e2e2cd97e2a1789692db21da49d24a142600ee81
robynlarsen <r@robynlarsen.ca>
2015-04-30 10:26:43 -0400

    Fix typo on activites

--- db/migrate/20150412191756_add_workout_id_to_activity.rb
+++ db/migrate/20150412191756_add_workout_id_to_activity.rb
@@ -1,6 +1,6 @@
 class AddWorkoutIdToActivity < ActiveRecord::Migration
   def change
     remove_foreign_key :workouts, :activities
-    add_foreign_key :activites, :workouts
+    add_foreign_key :activities, :workouts
   end
 end
