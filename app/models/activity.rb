e8e89c44c1a660ba6aee8dcc6895f0ad19f89ba0
robynlarsen <r@robynlarsen.ca>
2015-04-30 21:26:09 -0400

    Add uniqueness to activity name

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,5 +1,5 @@
 class Activity < ActiveRecord::Base
   belongs_to :workout #each activity only belongs to one workout?
   has_many :workouts, through: :workout_activities
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 end
