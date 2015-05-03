5d1d5e1b02dcb8e9693027aaeaf214db9ad17e40
robynlarsen <r@robynlarsen.ca>
2015-05-03 11:01:07 -0400

    Add username to user migration

---
+++ db/migrate/20150501010233_add_username_to_users.rb
@@ -0,0 +1,5 @@
+class AddUsernameToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :first_name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150428010349) do
+ActiveRecord::Schema.define(version: 20150501010233) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "name"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20150428010349) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
