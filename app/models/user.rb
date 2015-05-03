748da846cb530093909fc9f4ad07285035cae437
robynlarsen <r@robynlarsen.ca>
2015-05-03 11:16:25 -0400

    Add data id and index to workout new form

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,6 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # attr_accessible :email, :password, :password_confirmation, :remember_me, :name
 end
