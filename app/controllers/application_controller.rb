cfef0429aa14cd41c1f2555c7df81c1e09cace82
robynlarsen <r@robynlarsen.ca>
2015-05-05 11:57:21 -0400

    Add user first name to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,22 @@ class ApplicationController < ActionController::Base
   # Devise - force the user to redirect to the login page if the user was not logged in
   # TODO - comment out the line below for the root page login
   before_action :authenticate_user!, :except => [:home]
+
+  before_action :configure_devise_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_devise_permitted_parameters
+    registration_params = [:first_name, :email, :password, :password_confirmation]
+
+    if params[:action] == 'update'
+      devise_parameter_sanitizer.for(:account_update) {
+        |u| u.permit(registration_params << :current_password)
+      }
+    elsif params[:action] == 'create'
+      devise_parameter_sanitizer.for(:sign_up) {
+        |u| u.permit(registration_params)
+      }
+    end
+  end
 end
