aaef67223a360a63cb849068994a1c9a8d9d6c88
robynlarsen <r@robynlarsen.ca>
2015-05-06 09:03:15 -0400

    Add user id to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150501010233) do
+ActiveRecord::Schema.define(version: 20150506124639) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "name"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20150501010233) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "first_name"
+    t.integer  "user_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
